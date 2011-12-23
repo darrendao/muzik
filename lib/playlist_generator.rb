4f2e1073538d6fc6a57503452834849b04935cf9
unknown <dev@ddao-PC.(none)>
2011-12-22 23:38:23 -0800

    Fix playlist generator. Update populate db script to have better data

--- lib/playlist_generator.rb
+++ lib/playlist_generator.rb
@@ -4,7 +4,7 @@ class PlaylistGenerator
   end
   def generate(group, start_time = 0, end_time = 86400)
     playlist = {}
-    songs = group.songs
+    songs = group.songs.clone
     songs_counter = songs.size
     current_time = start_time              # seconds since midnight
     current_hour = start_time / (60*60)    # hours since midnight
@@ -15,11 +15,12 @@ class PlaylistGenerator
       puts "Current energy level is #{current_time_energy_level.inspect}"
       songs_counter = songs_counter - 1
       if songs_counter < 0
-        raise "CRAP... NO MORE SONG. CAN'T CREATE PLAYLIST"
+        puts "CRAP... NO MORE SONG. CAN'T CREATE PLAYLIST."
+        break
       end
 
       # Don't play songs of the same artist or album that have been played during the last hour
-      if current_hour_songs.any?{|song| song.artist == candidate.artist or song.album == candidate.album}
+      if current_hour_songs.any?{|song| song.artist == candidate.artist or song.belongs_to_album == candidate.belongs_to_album}
         puts "Skipping #{candidate.title} because of artist or album conflict"
         songs.push(candidate)
         next
@@ -29,7 +30,7 @@ class PlaylistGenerator
       # If no energy level specified for current time, then we don't care
       song_energy_level = group.energy_level_of(candidate.id)
       if current_time_energy_level && current_time_energy_level != song_energy_level
-        puts "Skipping #{candidate.title} because of energy level conflict. Song energy level is #{song_energy_level.inspect}"
+        puts "Skipping #{candidate.title} because of energy level conflict. Song energy level is #{song_energy_level.name}"
         songs.push(candidate)
         next
       end
