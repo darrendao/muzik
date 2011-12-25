898adf0be712518edd68201d6feebc2a6b42f076
unknown <dev@ddao-PC.(none)>
2011-12-24 22:08:37 -0800

    Add default dashboard page. Just empty for now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Muzik::Application.routes.draw do
+  resources :dashboard
   resources :group_holiday_schedules
 
   resources :playlists do
@@ -95,6 +96,7 @@ Muzik::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
