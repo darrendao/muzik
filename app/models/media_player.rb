2851d60d72956e93fdd6b207d60abdd70dae7b47
unknown <dev@ddao-PC.(none)>
2012-01-01 10:11:04 -0800

    Less restriction on the media player model

--- app/models/media_player.rb
+++ app/models/media_player.rb
@@ -1,8 +1,9 @@
 class MediaPlayer < ActiveRecord::Base
   belongs_to :location
-  validates :hostname, :uniqueness => true, :presence => true
-  validates :serial, :uniqueness => true, :presence => true
+  validates :hostname, :uniqueness => true
+  validates :serial, :uniqueness => true
   validates :location_id, :uniqueness => true, :unless => :empty_location_id?
+
   def empty_location_id?
     return location_id.nil?
   end
