b08ec1d8c825ff297a7edcc66cc9f5a4b4026f50
unknown <dev@ddao-PC.(none)>
2012-01-01 10:24:45 -0800

    Blah.. changing validation for media player and location

--- app/models/media_player.rb
+++ app/models/media_player.rb
@@ -1,11 +1,15 @@
 class MediaPlayer < ActiveRecord::Base
   belongs_to :location
   validates :hostname, :uniqueness => true
-  validates :serial, :uniqueness => true
+  validates :serial, :uniqueness => true, :unless => :empty_serial?
   validates :location_id, :uniqueness => true, :unless => :empty_location_id?
 
   def empty_location_id?
     return location_id.nil?
+  end
+
+  def empty_serial?
+    return (serial.nil? or serial.empty?)
   end
   def portal_url
     if ip_address && !ip_address.empty?
