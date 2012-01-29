faac6cb9051e86ce743734c5feb16243a84ab385
unknown <dev@ddao-PC.(none)>
2012-01-28 18:26:29 -0800

    sort energy level by name

--- app/models/energy_level.rb
+++ app/models/energy_level.rb
@@ -1,5 +1,6 @@
 class EnergyLevel < ActiveRecord::Base
   has_many :energy_level_intervals, :dependent => :destroy
+
   def self.non_holiday
     EnergyLevel.order('name').select{|e| e.name !~ /^H\d/}
   end
